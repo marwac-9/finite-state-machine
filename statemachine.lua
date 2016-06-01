4bb0c5eecb2c0a064ebd7f24e58b51be6258a283
mario <mario@localhost>
2016-06-01 19:10:27 +0200

    wrapped the StateMachine in a function and added the global state

--- statemachine.lua
+++ statemachine.lua
@@ -1,44 +1,48 @@
-local StateMachine = {}
+function createStateMachine()
+	local StateMachine = {}
 
 
-function StateMachine:new(entity, currentState, previousState)
-	--only entity and currentState required
-	local stateMachine = {}
-	stateMachine.curState = currentState
-	stateMachine.prevState = previousState
-	
-	local states = {}
+	function StateMachine:new(entity, currentState, previousState, globalState)
+		--only entity and currentState required
+		local stateMachine = {}
+		stateMachine.curState = currentState
+		stateMachine.prevState = previousState
+		stateMachine.globalState = globalState
+		
+		local states = {}
 
-	states[currentState.name] = currentState
-	if previousState then
-		states[previousState.name] = previousState
+		states[currentState.name] = currentState
+		states[globalState.name] = globalState
+		if previousState then
+			states[previousState.name] = previousState
+		end
+		function stateMachine:update()
+			self.globalState.execute(entity)
+			self.curState.execute(entity)
+		end
+
+		function stateMachine:addState(newState)
+			states[newState.name] = newState
+		end
+
+		function stateMachine:changeState(newStateName)
+			self.prevState = self.curState
+			self.curState.exit(entity)
+			self.curState = states[newStateName]
+			self.curState.enter(entity)
+		end
+
+		function stateMachine:revertState()
+			self:changeState(self.prevState.name)
+		end
+
+		function stateMachine:isInState(stateName)
+			return stateName == self.curState.name
+		end
+
+		return stateMachine
+
 	end
-	function stateMachine:update()
-		self.curState.execute(entity)
-		-- print(self.curState.name)
-	end
 
-	function stateMachine:addState(newState)
-		states[newState.name] = newState
-	end
-
-	function stateMachine:changeState(newStateName)
-		self.prevState = self.curState
-		self.curState.exit(entity)
-		self.curState = states[newStateName]
-		self.curState.enter(entity)
-	end
-
-	function stateMachine:revertState()
-		self:changeState(self.prevState.name)
-	end
-
-	function stateMachine:isInState(stateName)
-		return stateName == self.curState.name
-	end
-
-	return stateMachine
-
+	return StateMachine
 end
-
-return StateMachine
